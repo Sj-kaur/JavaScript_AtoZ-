0a3626ed7b2eab1f7482ccb2188889baa3f5ecfb
Sj-kaur <90175679+Sj-kaur@users.noreply.github.com>
2025-04-27 10:32:36 +0000

    feat: Add basic JavaScript files for conversion operations

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33a"
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" can convert to => 33
+// "33abc" converts to => NaN(not a number)
+// true => 1; false => 0
+
+let isLoggedIn = "Simar"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "simar" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
